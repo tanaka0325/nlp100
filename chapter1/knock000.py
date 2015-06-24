ce6c85ae14d03fc85085d6c5e8a525650ab8a313
h.tanaka <h.tanaka.0325@gmail.com>
2015-06-25 01:15:16 +0900

    done 000

---
+++ chapter1/knock000.py
@@ -0,0 +1,14 @@
+# 00. 文字列の逆順
+# 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+
+s = "stressed"
+
+def reverse(s :str) -> str:
+    list = []
+
+    for char in s:
+        list.insert(0, char)
+
+    return "".join(list)
+
+print(reverse(s))
